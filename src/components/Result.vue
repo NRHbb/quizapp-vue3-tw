0825de54686cc46e4f45369f10b5494bc4271486
NR Habibi <nrhbb14@gmail.com>
2023-12-12 11:32:05 +0800

    create result component and finishing the quizapp

---
+++ src/components/Result.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="results"
+    class="text-center py-[50px] px-0"
+    >
+        <p class="text-2xl">Your Results is ....</p>
+        <h1 class="text-6xl font-bold pb-5">
+            {{numberCorrect}}/{{quizLength}}
+        </h1>
+
+        <RouterLink to="/">
+            Go Home
+        </RouterLink>
+
+    </div>
+</template>
+
+<script setup>
+import { defineProps} from 'vue';
+import { RouterLink } from 'vue-router';
+
+
+const { quizLength, numberCorrect } = defineProps([
+    'quizLength', 'numberCorrect'
+])
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
