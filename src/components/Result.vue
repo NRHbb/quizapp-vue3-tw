272b0a4f4910401bf5db545c6ccb57d475570508
NR Habibi <nrhbb14@gmail.com>
2023-12-12 11:33:48 +0800

    add some styling

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -2,13 +2,13 @@
     <div id="results"
     class="text-center py-[50px] px-0"
     >
-        <p class="text-2xl">Your Results is ....</p>
+        <p class="text-2xl pb-3">Your Results is ....</p>
         <h1 class="text-6xl font-bold pb-5">
             {{numberCorrect}}/{{quizLength}}
         </h1>
 
         <RouterLink to="/">
-            Go Home
+            <p class="text-xl font-medium">Go Home</p>
         </RouterLink>
 
     </div>
